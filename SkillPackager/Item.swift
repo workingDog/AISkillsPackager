3b5c271c48fe1d63364985674e8a0b5c323b21b0
workingdog <ringowathelet@gmail.com>
2026-04-30 16:39:39 +0900

    Initial Commit

---
+++ SkillPackager/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SkillPackager
+//
+//  Created by Ringo Wathelet on 2026/04/30.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
